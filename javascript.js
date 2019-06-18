3c1d01917ddbc754bfe509c225fbaddf0f73fb9e
ashlie <ashlie86.2003@yahoo.com>
2019-06-18 16:54:18 -0400

    still broken idk what to do

--- javascript.js
+++ javascript.js
@@ -1,22 +1,90 @@
+//I'm thinking the website that walked me through this step by step taught me the wrong things.//
 
 //Setting countdown clock for whole game, 600 secs in 10mins//
 setTimeout(timeUp, 1000 * 600);
 
 //Adding reset button//
-trivia.reset();
-      document.getElementById("trivia-reset").addEventListener("click", trivia.reset);
-      document.getElementById("trivia-reset").disabled = false;
+    //function reset()
+    //  document.getElementById("trivia-reset").addEventListener("click", reset);
+     // document.getElementById("trivia-reset").disabled = false;
     
 
-    reset : function () {
+    function reset() {
         trivia.rights = 0;
         trivia.wrongs = 0;
         trivia.elLives.innerHTML = trivia.guesses;
         trivia.elImg.style.opacity = 0;}
   
-     const triviaContainer = document.getElementById('trivia'); 
-     const resultsContainer = document.getElementById('results');
-     const submitButton = document.getElementById('submit');
+     
+
+        function buildTrivia(){
+            const output = [];
+
+            myQuestions.forEach(
+                (currentQuestion, questionNumber) => {
+                    const answers = [];
+                    for(letter in currentQuestion.answers){
+                        const answers = [];
+
+                        // and for each available answer...
+                        for(letter in currentQuestion.answers){
+                        
+                          // ...add an html radio button
+                          answers.push(
+                            `<label>
+                              <input type="radio" name="question${questionNumber}" value="${letter}">
+                              ${letter} :
+                              ${currentQuestion.answers[letter]}
+                            </label>`
+                          );
+                        }
+                  
+                        // add this question and its answers to the output
+                        output.push(
+                          `<div class="question"> ${currentQuestion.question} </div>
+                          <div class="answers"> ${answers.join("")} </div>`
+                        );
+                      };
+                  
+                      // finally combine our output list into one string of HTML and put it on the page
+                      quizContainer.innerHTML = output.join("");
+                    
+                  
+                    function showResults() {
+                      // gather answer containers from our quiz
+                      const answerContainers = quizContainer.querySelectorAll(".answers");
+                  
+                      // keep track of user's answers
+                      let numCorrect = 0;
+                  
+                      // for each question...
+                      myQuestions.forEach((currentQuestion, questionNumber) => {
+                        // find selected answer
+                        const answerContainer = answerContainers[questionNumber];
+                        const selector = `input[name=question${questionNumber}]:checked`;
+                        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+                  
+                        // if answer is correct
+                        if (userAnswer === currentQuestion.correctAnswer) {
+                          // add to the number of correct answers
+                          numCorrect++;
+                  
+                          // color the answers green
+                          answerContainers[questionNumber].style.color = "lightgreen";
+                        } else {
+                          // if answer is wrong or blank
+                          // color the answers red
+                          answerContainers[questionNumber].style.color = "red";
+                        }
+                      });
+                  
+                      // show number of correct answers out of total
+                      resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+                    }
+
+                    const quizContainer = document.getElementById('quiz'); 
+                    const resultsContainer = document.getElementById('results');
+                    const submitButton = document.getElementById('submit');
 
         function buildTrivia(){}
         function showResults(){}
@@ -116,19 +184,16 @@ trivia.reset();
             correctAnswer: "a",    
         },    
         ];//last one, ends array//
+                  
+                    // display quiz right away
+                    buildQuiz();
+                    
+                  
+                    // on submit, show results
+                    submitButton.addEventListener("click", showResults);
+                  })();
 
-        function buildTrivia(){
-            const output = [];
-
-            myQuestions.forEach(
-                (currentQuestion, questionNumber) => {
-                    const answers = [];
-                    for(letter in currentQuestion.answers){
-                        answers.push(
-            
-                }
-                )
-        }
+                           
 
 
 
@@ -136,4 +201,4 @@ trivia.reset();
 
 
 
-      window.addEventListener("load", trivia.init); 
+      window.addEventListener("load", trivia.init)}; 
